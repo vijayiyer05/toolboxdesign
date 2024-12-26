0ff051b12fc0dc61c62c5808525e7f23f84361b1
Vijay Iyer <viyer05@gmail.com>
2024-12-26 13:39:08 -0500

    Update setupToolbox.m
    Added information about "how to use" including where the function should be located & called

--- setupToolbox.m
+++ setupToolbox.m
@@ -1,3 +1,5 @@
-% One-line script which adds /toolbox folder contents (including subfolders) to path for current MATLAB session
-% Key use case: enables specific live scripts in /examples folder opened to via Open in MATLAB Online badge to utilize library code (i.e., in /toolbox folder), for simple toolboxes (no dependencies) not currently using package management. 
+% One-line script for live scripts in /examples folder, which adds /toolbox folder contents (including subfolders) to path for current MATLAB session
+% Driving use case: enabling showcase live scripts located directly in the /examples folder to be opened to via the Open in MATLAB Online badge and then run through without installation 
+% How to use: 1) place this function in the /examples folder and 2) call this script at the top of all showcase example live scripts
+% Limitations: only works for toolboxes without dependencies (for such cases, toolbox packaging is required)
 addpath(genpath("../../toolbox"));
